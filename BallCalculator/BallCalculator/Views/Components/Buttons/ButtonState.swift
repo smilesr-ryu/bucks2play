1ebebc32c8a572c656809dcffa863635a45df1d9
Yunki-H <yunki.dev@gmail.com>
2025-05-05 19:27:59 +0900

    chore: ButtonStyle

---
+++ BallCalculator/BallCalculator/Views/Components/Buttons/ButtonState.swift
@@ -0,0 +1,12 @@
+//
+//  ButtonState.swift
+//  BallCalculator
+//
+//  Created by Yunki on 5/5/25.
+//
+
+enum ButtonState {
+    case normal
+    case pressed
+    case disabled
+}
---
+++ BallCalculator/BallCalculator/Views/Components/Buttons/ButtonStyleType.swift
@@ -0,0 +1,12 @@
+//
+//  ButtonStyleType.swift
+//  BallCalculator
+//
+//  Created by Yunki on 5/5/25.
+//
+
+enum ButtonStyleType {
+    case primary
+    case secondary
+    case tertiary
+}

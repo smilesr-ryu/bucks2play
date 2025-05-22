d99e3d1ecf7824c6b826cc83291357173174fc8a
Yunki-H <yunki.dev@gmail.com>
2025-05-22 21:48:43 +0900

    feat: Theme Select View Update

---
+++ BallCalculator/BallCalculator/Manager/PopupManager.swift
@@ -0,0 +1,8 @@
+//
+//  PopupManager.swift
+//  BallCalculator
+//
+//  Created by Yunki on 5/22/25.
+//
+
+import Foundation
--- BallCalculator/BallCalculator/Views/ThemeSelectView.swift
+++ BallCalculator/BallCalculator/Views/ThemeSelectView.swift
@@ -20,103 +20,138 @@ struct ThemeSelectView: View {
     
     
     var body: some View {
-        VStack(spacing: 10) {
-            HStack {
-                VStack(alignment: .leading) {
-                    Text("자동설정")
-                        .font(Font.custom("NotoSansKR-Bold", size: 14))
-                        .bold()
+        VStack(spacing: 0) {
+            Button {
+                
+            } label: {
+                HStack {
+                    HStack(alignment: .firstTextBaseline, spacing: 4) {
+                        Text("로그인")
+                            .fontStyle(.display2_B)
+                        Text("하고 모든 테마를 열어보세요!")
+                            .fontStyle(.body1_R)
+                    }
                     
                     
-                    Text("해당기간 리그가 자동 변경됩니다.")
-                        .font(Font.custom("NotoSansKR-Regular", size: 13))
-                        .foregroundColor(Color(red: 0.34, green: 0.34, blue: 0.34))
+                    Spacer()
+                    
+                    Image(.arrowRight24)
+                }
+                .foregroundStyle(.black01)
+                .padding(.horizontal, 16)
+                .padding(.vertical, 20)
+            }
+            
+            Rectangle()
+                .foregroundStyle(.black04)
+                .frame(height: 1)
+                .padding(.horizontal, 20)
+                .padding(.top, 12)
+            
+            VStack(spacing: 16) {
+                HStack {
+                    VStack(alignment: .leading, spacing: 2) {
+                        Text("자동설정")
+                            .fontStyle(.body1_B)
+                        
+                        Text("해당기간 리그가 자동 변경됩니다.")
+                            .fontStyle(.label1_R)
+                            .foregroundColor(.black02)
+                    }
+                    
+                    Spacer()
+                    
+                    TinyButton("기간설정", type: .secondary) {
+                        
+                    }
                 }
                 
-                Spacer()
-                
-                VStack(spacing: 10) {
+                HStack {
                     Button(action: {
-                        isOnAutoTheme.toggle()
+                        if let todayTheme = themeManager.themeForToday() {
+                            // 만약 오늘에 해당하는 테마가 있으면 그 테마로 변경
+                            currentTheme = todayTheme
+                            UserDefaults.standard.set(currentTheme.rawValue, forKey: "theme")
+                        }
                     }) {
-                        Text("기간설정")
-                            .font(Font.custom("NotoSansKR-Regular", size: 13))
-                            .bold()
-                            .foregroundColor(Color(red: 0.34, green: 0.34, blue: 0.34))
+                        Rectangle()
+                            .foregroundColor(.clear)
+                            .frame(width: 70, height: 70)
                             .background(
-                                Rectangle()
-                                    .foregroundColor(.clear)
-                                    .frame(width: 76, height: 28)
-                                    .background(Color(red: 0.93, green: 0.93, blue: 0.93))
-                                    .cornerRadius(55)
+                                AngularGradient(colors: Gradients, center: .center)
+                            )
+                            .cornerRadius(10)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .inset(by: 0.75)
+                                    .stroke(Color(red: 0.93, green: 0.93, blue: 0.93), lineWidth: 1.5)
                             )
                     }
+                    Spacer()
                 }
             }
+            .padding(.horizontal, 20)
+            .padding(.vertical, 16)
             
-            HStack {
-                Button(action: {
-                    if let todayTheme = themeManager.themeForToday() {
-                        // 만약 오늘에 해당하는 테마가 있으면 그 테마로 변경
-                        currentTheme = todayTheme
-                        UserDefaults.standard.set(currentTheme.rawValue, forKey: "theme")
-                    }
-                }) {
-                    Rectangle()
-                        .foregroundColor(.clear)
-                        .frame(width: 70, height: 70)
-                        .background(
-                            AngularGradient(colors: Gradients, center: .center)
-                        )
-                        .cornerRadius(10)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 10)
-                                .inset(by: 0.75)
-                                .stroke(Color(red: 0.93, green: 0.93, blue: 0.93), lineWidth: 1.5)
-                        )
-                }
-                Spacer()
-            }
-            
-            HStack {
-                Text("테마선택")
-                    .font(Font.custom("NotoSansKR-Bold", size: 14))
-                Spacer()
-            }
-            
-            HStack(spacing: 15) {
-                ForEach(Theme.allCases, id: \.self) { theme in
-                    Button {
-                        currentTheme = theme
-                        UserDefaults.standard.set(theme.rawValue, forKey: "theme")
-                    } label: {
-                        Image("logo_"+theme.rawValue)
-                            .resizable()
-                            .scaledToFit()
-                            .padding(5)
-                            .frame(width: 70, height: 70)
-                            .background {
-                                Rectangle()
-                                    .foregroundColor(.clear)
-                                    .frame(width: 70, height: 70)
-                                    .background(.white)
-                                    .cornerRadius(10)
-                                    .overlay(
-                                        RoundedRectangle(cornerRadius: 10)
-                                            .inset(by: 0.75)
-                                            .stroke(Color(red: 0.93, green: 0.93, blue: 0.93), lineWidth: 1.5)
-                                    )
-                            }
+            VStack(spacing: 16) {
+                HStack {
+                    VStack(alignment: .leading, spacing: 2) {
+                        Text("테마선택")
+                            .fontStyle(.body1_B)
+                        
+                        Text("로그인하면 테마가 열려요!")
+                            .fontStyle(.label1_R)
+                            .foregroundColor(.black02)
                     }
                     
+                    Spacer()
                     
+                    TinyButton("작성하기", type: .secondary) {
+                        
+                    }
                 }
+                
+                HStack {
+                    ForEach(Theme.allCases, id: \.self) { theme in
+                        Button {
+                            currentTheme = theme
+                            UserDefaults.standard.set(theme.rawValue, forKey: "theme")
+                        } label: {
+                            Image("logo_"+theme.rawValue)
+                                .resizable()
+                                .scaledToFit()
+                                .padding(5)
+                                .frame(width: 70, height: 70)
+                                .background {
+                                    Rectangle()
+                                        .foregroundColor(.clear)
+                                        .frame(width: 70, height: 70)
+                                        .background(.white)
+                                        .cornerRadius(10)
+                                        .overlay(
+                                            RoundedRectangle(cornerRadius: 10)
+                                                .inset(by: 0.75)
+                                                .stroke(Color(red: 0.93, green: 0.93, blue: 0.93), lineWidth: 1.5)
+                                        )
+                                }
+                        }
+                        
+                        if Theme.allCases.last != theme {
+                            Spacer()
+                        }
+                    }
+                }
+                
             }
+            .padding(.horizontal, 20)
+            .padding(.vertical, 12)
         }
-        .padding(.horizontal, 35)
         .sheet(isPresented: $isOnAutoTheme) {
             AutoThemeSelectView(isOnAutoTheme: $isOnAutoTheme, startDate: Date(), endDate: Date())
         }
     }
 }
 
+#Preview {
+    ThemeSelectView(currentTheme: .constant(.aus))
+}

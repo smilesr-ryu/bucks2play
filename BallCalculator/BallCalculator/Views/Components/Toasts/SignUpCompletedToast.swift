6d730c78aa3bcda27a941768502ea4d3c5053331
Yunki-H <yunki.dev@gmail.com>
2025-05-23 15:56:48 +0900

    feat: Toast

---
+++ BallCalculator/BallCalculator/Views/Components/Toasts/SignUpCompletedToast.swift
@@ -0,0 +1,32 @@
+//
+//  SignUpCompletedToast.swift
+//  BallCalculator
+//
+//  Created by Yunki on 5/23/25.
+//
+
+import SwiftUI
+
+struct SignUpCompletedToast: View {
+    var body: some View {
+        HStack {
+            Text("회원가입이 완료되었습니다.")
+                .fontStyle(.label1_R)
+                .foregroundStyle(.white01)
+                .padding(.horizontal, 16)
+                .padding(.vertical, 14)
+            Spacer()
+        }
+        .background {
+            RoundedRectangle(cornerRadius: 12)
+                .foregroundStyle(.black)
+                .opacity(0.85)
+        }
+        .padding(.horizontal, 8)
+        .padding(.vertical, 12)
+    }
+}
+
+#Preview {
+    SignUpCompletedToast()
+}
